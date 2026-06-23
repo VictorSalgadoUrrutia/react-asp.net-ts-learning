fade00ef3de823dc476010e90d2de8b24a38c806
Victor Salgado <victor.vu583@gmail.com>
2026-06-23 12:44:24 -0500

    Ajustes finales de estructura

--- AuthBackend/Models/LoginDto.cs
+++
@@ -1,8 +0,0 @@
-namespace AuthBackend.Models
-{
-    public class LoginDto
-    {
-        public string Username { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
